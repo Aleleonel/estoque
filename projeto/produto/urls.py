3e105ba00773432ce15cb1f75fdf20472b7fc7e0
aleleonel <aleleonel@gmail.com>
2019-12-27 16:38:03 -0300

    form edit

--- projeto/produto/urls.py
+++ projeto/produto/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', v.produto_list, name='produto_list'),
     path('<int:pk>/', v.produto_detail, name='produto_detail'),
     path('add/', v.ProdutoCreate.as_view(), name='produto_add'),
-
+    path('<int:pk>/edit/', v.ProdutoUpdate.as_view(), name='produto_edit'),
 ]
--- projeto/produto/views.py
+++ projeto/produto/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from .models import Produto
 from .forms import ProdutoForm
 
@@ -29,3 +29,10 @@ class ProdutoCreate(CreateView):
     model = Produto
     template_name = 'produto_form.html'
     form_class = ProdutoForm
+
+
+class ProdutoUpdate(UpdateView):
+    model = Produto
+    template_name = 'produto_form.html'
+    form_class = ProdutoForm
+
