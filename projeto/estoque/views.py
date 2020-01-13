d85a7ac943a75e9049f291abe004a164ef7067c5
aleleonel <aleleonel@gmail.com>
2020-01-13 11:44:30 -0300

    refatorando estoque_list em Class Base View

--- projeto/estoque/views.py
+++ projeto/estoque/views.py
@@ -1,6 +1,7 @@
 from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, resolve_url
+from django.views.generic import ListView
 from projeto.produto.models import Produto
 from .models import Estoque, EstoqueEntrada, EstoqueItens, EstoqueSaida
 from .forms import EstoqueItensForm, EstoqueForm
@@ -15,6 +16,17 @@ def estoque_entrada_list(request):
         'url_add': 'estoque:estoque_entrada_add'
     }
     return render(request, template_name, context)
+
+
+class EstoqueEntradaList(ListView):
+    model = EstoqueEntrada
+    template_name = 'estoque_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(EstoqueEntradaList, self).get_context_data(**kwargs)
+        context['titulo'] = 'Entrada'
+        context['url_add'] = 'estoque:estoque_entrada_add'
+        return context
 
 
 def estoque_entrada_detail(request, pk):
@@ -89,6 +101,17 @@ def estoque_saida_list(request):
     return render(request, template_name, context)
 
 
+class EstoqueSaidaList(ListView):
+    model = EstoqueSaida
+    template_name = 'estoque_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(EstoqueSaidaList, self).get_context_data(**kwargs)
+        context['titulo'] = 'Saida'
+        context['url_add'] = 'estoque:estoque_saida_add'
+        return context
+
+
 def estoque_saida_detail(request, pk):
     template_name = 'estoque_detail.html'
     obj = EstoqueSaida.objects.get(pk=pk)
@@ -107,3 +130,4 @@ def estoque_saida_add(request):
     if context.get('pk'):
         return HttpResponseRedirect(resolve_url(url, context.get('pk')))
     return render(request, template_name, context)
+
