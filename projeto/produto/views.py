bf1f6f4e86c54ce7f15e1353a83b5902e12c0377
aleleonel <aleleonel@gmail.com>
2020-01-14 20:07:46 -0300

    Inserido um campo de busca em lista de produtos e reconfigurado template produto_list e a view produto_list request.GET.get()

--- projeto/produto/views.py
+++ projeto/produto/views.py
@@ -10,6 +10,9 @@ from .models import Produto
 def produto_list(request):
     template_name = 'produto_list.html'
     objects = Produto.objects.all()
+    search = request.GET.get('search')
+    if search:
+        objects = objects.filter(produto__icontains=search)
     context = {'object_list': objects}
     return render(request, template_name, context)
 
