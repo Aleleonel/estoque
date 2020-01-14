d4821eeb2b630f13240e177e4f47d3f4e43e6ee6
aleleonel <aleleonel@gmail.com>
2020-01-14 20:48:12 -0300

    configurado arquivo de paginação

--- projeto/produto/urls.py
+++ projeto/produto/urls.py
@@ -4,7 +4,7 @@ from projeto.produto import views as v
 app_name = 'produto'
 
 urlpatterns = [
-    path('', v.produto_list, name='produto_list'),
+    path('', v.ProdutoList.as_view(), name='produto_list'),
     path('<int:pk>/', v.produto_detail, name='produto_detail'),
     path('add/', v.ProdutoCreate.as_view(), name='produto_add'),
     path('<int:pk>/edit/', v.ProdutoUpdate.as_view(), name='produto_edit'),
